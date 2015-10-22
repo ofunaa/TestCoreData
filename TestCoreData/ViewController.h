6dd7398b41498857cc42eb1e6f39f0c7f1e38588
takujifunao <takuji.funao@gmail.com>
2015-10-22 12:44:29 +0900

    Initial Commit

---
+++ TestCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCoreData
+//
+//  Created by takuji funao on 2015/10/22.
+//  Copyright (c) 2015年 takuji funao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestCoreData
+//
+//  Created by takuji funao on 2015/10/22.
+//  Copyright (c) 2015年 takuji funao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
